5be3b6b39db4aca71176951180bd4aabc8904c6e
Eduardo Fernandes <eduardo.fernandes@guestcentric.com>
2019-11-22 17:16:43 +0000

    WIP: 2019-11-22 17:16

--- App/Bash/Inc/functions/phpCheck.sh
+++ App/Bash/Inc/functions/phpCheck.sh
@@ -8,11 +8,13 @@ function phpCheck()
             return 1
         fi
 
-        printf "\n\033[0;32mChecking ${1}\033[0m\n\n"
-        printf "\033[0;96m"
+        printf "\n\033[0;96mPHP -l ${1}\033[0m\n\n"
         php -l "${1}"
-        printf "\033[0m"
+        printf "\n\033[0;96mPHPMD ${1}\033[0m\n\n"
         phpmd "${1}" text "${HOME}/Shell/Standards/PhpMd/phpms.guestcentric.xml"
+        printf "\n"
+        printf "\n\033[0;96mPHAN ${1}\033[0m\n\n"
+        phan --color --config-file "${HOME}/Shell/Standards/Phan/config.php" "${1}"
         printf "\n"
         return 0
     fi
@@ -28,11 +30,14 @@ function phpCheck()
         if [[ "${file}" != *.php ]]; then
             continue
         fi
-        printf "\n\033[0;32mChecking ${file}\033[0m\n\n"
-        printf "\033[0;96m"
+        printf "\n\033[0;96mPHP -l ${1}\033[0m\n\n"
         php -l "${file}"
-        printf "\033[0m"
+        printf "\n"
+        printf "\n\033[0;96mPHPMD -l ${1}\033[0m\n\n"
         phpmd "${file}" text "${HOME}/Shell/Standards/PhpMd/phpms.guestcentric.xml"
+        printf "\n"
+        printf "\n\033[0;96mPHAN -l ${1}\033[0m\n\n"
+        phan --color --config-file "${HOME}/Shell/Standards/Phan/config.php" "${file}"
         printf "\n"
     done
 }
